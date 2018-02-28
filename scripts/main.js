5dfcd0089a154bdbe0de082d697acda8d86794b4
Dylan Bailey <dylanba1@gmail.com>
2018-02-28 13:33:28 -0500

    Streamlined code -

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,14 @@
-form.addEventListner('submit', function(event) {
+var coffeeOrders = [];
+var form = document.querySelector('body > section > div > div > form');
+
+form.addEventListener('submit', function (event) {
     event.preventDefault();
-    coffeeOrders.push(form.fullName)
+    coffeeOrders.push({
+        'Coffee': form.coffee.value, 'Email': form.emailAddress.value,
+        'Size': form.size.value, 'Flavor': form.flavor.value, 'Strength': form.strength.value
+    });
+    form.reset();
 })
+
+console.log(coffeeOrders)
+document.getElementById("lastForm").appendChild(coffeeOrders)
